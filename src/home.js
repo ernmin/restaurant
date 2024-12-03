5986f886e958ac85661d62598cfa2a3d5de5c8fd
ernmin <42770608+ernmin@users.noreply.github.com>
2024-12-03 12:49:11 +0800

    Add CSS to page Change appearance of Tab buttons Change background color Change logo size Add media query for logo Append Welcome Message Under logo

--- src/home.js
+++ src/home.js
@@ -1,9 +1,17 @@
 export function image_logo(logo) {
     const image = document.createElement("img");
     image.src = logo;
+    image.id = 'logo';
     const content = document.querySelector('#content');
-
     content.appendChild(image);
+    const welcome = document.createElement("div");
+    welcome.textContent = "Welcome to Assembly Ground";
+    content.appendChild(welcome);
+    //content.textContent = "Welcome to Assembly Ground";
+}
+
+export function description() {
+
 }
 
 //https://www.theassemblyground.com/
