40e080b6d8467cf10f6a09924459fd22e37df45f
ernmin <42770608+ernmin@users.noreply.github.com>
2024-12-03 13:06:11 +0800

    Add Main Image and Caption Complete Homepage

--- src/home.js
+++ src/home.js
@@ -5,13 +5,21 @@ export function image_logo(logo) {
     const content = document.querySelector('#content');
     content.appendChild(image);
     const welcome = document.createElement("div");
-    welcome.textContent = "Welcome to Assembly Ground";
+    welcome.textContent = "Welcome to The Assembly Ground";
     content.appendChild(welcome);
     //content.textContent = "Welcome to Assembly Ground";
 }
 
-export function description() {
-
+export function image_food(food) {
+    const food_image = document.createElement("img");
+    food_image.src = food;
+    food_image.id = 'food';
+    const content = document.querySelector('#content');
+    content.appendChild(food_image);
+    const caption = document.createElement("div");
+    caption.classList.add('caption');
+    caption.textContent = "Birthplace of all true Foodsmiths";
+    content.appendChild(caption);
 }
 
 //https://www.theassemblyground.com/
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
-import logo from "./AssemblyGround_logo.png"
-import { image_logo } from './home.js';
+import logo from "./AssemblyGround_logo.png";
+import food from "./poetry-and-food.jpeg";
+import { image_logo, image_food } from './home.js';
 
 image_logo(logo);
-
-alert('Testing');
+image_food(food);
